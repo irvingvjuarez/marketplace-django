b6aecd5bb4fbebda477ac7c3e437f95d66ef10aa
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-27 11:16:10 -0600

    Category/detail templated and route done

--- puddle/core/urls.py
+++ puddle/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, contact, browse
+from .views import index, contact, browse, category_detail
 
 urlpatterns = [
     path('', index, name="index"),
     path("contact/", contact, name="contact"),
-    path("browse/", browse, name="browse")
+    path("browse/", browse, name="browse"),
+    path("category/<int:category_id>", category_detail, name="category_detail")
 ]
--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from item.models import Category
+from item.models import Category, Item
 
 # Create your views here.
 def index(request):
@@ -15,4 +15,13 @@ def browse(request):
 
     return render(request, "core/browse.html", {
         "categories": allCategories
+    })
+
+def category_detail(request, category_id):
+    chosenCategory = Category.objects.get(id=category_id)
+    items = Item.objects.filter(category=chosenCategory)
+
+    return render(request, "core/category.html", {
+        "category_name": chosenCategory.name,
+        "items": items
     })
