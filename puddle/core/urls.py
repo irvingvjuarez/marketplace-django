87acb77ac9390101270f2ee5240b68f7c624344b
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-27 10:47:19 -0600

    Browse template donde

--- puddle/core/urls.py
+++ puddle/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, contact
+from .views import index, contact, browse
 
 urlpatterns = [
     path('', index, name="index"),
-    path("contact/", contact, name="contact")
+    path("contact/", contact, name="contact"),
+    path("browse/", browse, name="browse")
 ]
--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from item.models import Category
 
 # Create your views here.
 def index(request):
@@ -8,3 +9,10 @@ def index(request):
 
 def contact(request):
     return render(request, "core/contact.html")
+
+def browse(request):
+    allCategories = Category.objects.all()
+
+    return render(request, "core/browse.html", {
+        "categories": allCategories
+    })
