0df9bca2ed98f6bcc697b39c8086b43ba6fec1e0
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-26 21:50:23 -0600

    Item model added

--- puddle/item/models.py
+++ puddle/item/models.py
@@ -3,3 +3,24 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ("name",)
+        verbose_name_plural = "Categories"
+    
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    is_sold = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now_add=True)
+
+    category = Category
+
+    def __str__(self):
+        return self.name
+    
