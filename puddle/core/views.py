b0bed4029829a461d936423b335a00410783d173
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-27 11:48:57 -0600

    Front page styled already

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -3,8 +3,12 @@ from item.models import Category, Item
 
 # Create your views here.
 def index(request):
+    items = Item.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+
     return render(request, "core/index.html", {
-        "message": "Hello World"
+        "items": items,
+        "categories": categories
     })
 
 def contact(request):
