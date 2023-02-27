db25a14944e6510ff494924abdb15b31be195f8d
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-27 13:11:04 -0600

    Detail page for the item polished up

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -4,7 +4,11 @@ from .models import Item
 # Create your views here.
 def detail(request, item_id):
     item = get_object_or_404(Item, id=item_id)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(id=item_id)[0:3]
+    are_related_items = True if len(related_items) else False
 
     return render(request, "item/detail.html", {
-        "item": item
+        "item": item,
+        "related_items": related_items,
+        "are_related_items": are_related_items
     })
