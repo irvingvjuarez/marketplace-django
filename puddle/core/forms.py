630e0170dc0b977560c3392f4d1cf0c7791e233e
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-27 15:21:18 -0600

    Signup page working alredy

--- puddle/core/forms.py
+++ puddle/core/forms.py
@@ -6,3 +6,21 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+    
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        "placeholder": "Ej. Jesse Pinkman",
+        "class": "w-full p-2 outline-none rounded-xl"
+    }))
+
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        "placeholder": "Ej. email@example.com",
+        "class": "w-full p-2 outline-none rounded-xl"
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        "class": "w-full p-2 outline-none rounded-xl"
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        "class": "w-full p-2 outline-none rounded-xl"
+    }))
--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Category, Item
+from .forms import SignupForm
 
 # Create your views here.
 def index(request):
@@ -31,4 +32,16 @@ def category_detail(request, category_id):
     })
 
 def sign_up(request):
-    return render(request, "core/signup.html")
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("/login/")
+    else:
+        form = SignupForm()
+
+    return render(request, "core/signup.html", {
+        "form": form
+    })
